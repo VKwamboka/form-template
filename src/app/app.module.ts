c6dd8add6b5d167dba4abcb53cc9eee8996729ba
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-21 12:59:39 +0300

    updated code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+
+import { Component, ElementRef, AfterViewInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { ReactiveComponent } from './reactive/reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/template/template.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, AfterViewInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent {
+  default='default'
+  genders=['Male', 'Female']
+  @ViewChild('form') form!:NgForm
+
+  submitForm(){
+    console.log(this.form);
+    this.form.reset()
+  }
+  
+  prePopulate(){
+    this.form.setValue({
+      gender:"Male",
+      role:"FSJS",
+      message:"Hello an a Js developer",
+      personalDetails:{
+        email: "test@gmail.com",
+        phone:"072351690",
+        username: "John Doe",
+      }
+    })
+    // this.form.form.patchValue({
+    //   personalDetails:{
+    //         email: "test@gmail.com",
+    //         phone:"072351690",
+    //         username: "John Doe",
+    //       }
+    // })
+  }
+}
